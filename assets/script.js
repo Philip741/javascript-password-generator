02a33858d89cbb27a2ab52f3b15582e5510f0320
Philip Browning <philip741@gmail.com>
2021-08-18 21:03:58 -0400

    Removed default vars at top of script Added userPrompts function Added generatePassword function Changed writePassword function to call userPrompts function

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,3 @@
-var passLen = 15;
-var min = 1;
-var max = 10;
-var min_length = 8;
-var max_length = 128;
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -13,7 +6,7 @@ function randomNumber(min, max) {
 } 
 
 function randomLetter(upper) {
-  var alphabet = "abcdefghijklmnopqrstuvwxyz"
+  let alphabet = "abcdefghijklmnopqrstuvwxyz"
   if (upper) {
     alphabet = alphabet.toUpperCase();
     pickLetter = alphabet[Math.floor(Math.random() * alphabet.length)]
@@ -26,35 +19,90 @@ function randomLetter(upper) {
 }
 
 function randomSpecial() {
+  //generate random character from string and return
   const specChars = "#$%!";
   picChar = specChars[Math.floor(Math.random() * specChars.length)]
   return picChar
 }
 
-function generatePassword(passLen) {
-  var myPass = '';
-  var inclNum = confirm('Include numbers in password ? (press ok or cancel)');
-  var inclLetter = confirm('Include letters in password ? (press ok or cancel)');
-  var inclSpecial = confirm('Include special characters (eg. #!%$) in password ? (press ok or cancel)');
-  while (myPass.length < passLen) {
-      myPass += randomLetter(upper=true);
-      myPass += randomNumber(1,10);
-      myPass += randomSpecial();
-      myPass += randomLetter(upper=false); 
+function userPrompts() {
+  let genPass = '';
+  promptCheck = [];
+  userParams = [];
+  var passLen = prompt("Please enter the length of your password (between 8 and 128 characters)");
+  // Check pass length and accept or reject
+  if (passLen < 8 || passLen > 128) {
+    alert("Password length must be greater than 8 characters and less than 128 Characters")
+    return "Please try again"
+  } 
+  else {
+      passLen = parseInt(passLen);
   }
-  console.log(myPass);
-  return myPass
+  //parameter prompts
+  var Num = confirm('Include numbers in password ? (press ok or cancel)');
+  var Letter = confirm('Include letters in password ? (press ok or cancel)');
+  var Special = confirm('Include special characters in password(eg. #!%$) in password ? (press ok or cancel)');
+  var LetterUpper = confirm('Include upper case Letters in password ? (press ok or cancel)');
+  if (Num){ 
+    userParams.push('randomNumber');
+    genPass += randomNumber(1,9);
+   }
+  if (Letter){ 
+    userParams.push('randomLetter'); 
+    genPass += randomLetter(upper=false);
+  }
+  if (Special){ 
+    userParams.push('randomSpecial'); 
+    genPass += randomSpecial();
+  }
+  if (LetterUpper){ 
+    userParams.push('randomLetterUpper'); 
+    genPass += randomLetter(upper=true);
+  }
+  console.log(genPass)
+  promptCheck.push(Num,Letter,Special,LetterUpper);
+  // check if all prompt values are false if so prompt that at least one choice has to be made
+  if (promptCheck.every(function(item){ return item == false;})) {
+    alert("Please choose at least one type for your password. (eg. Include numbers and letters")
+    return "Please try again"
+  }
+  else{
+    var myPass = generatePassword(userParams,passLen,genPass);
+    return myPass
+  }
+}
+function generatePassword(params,passLen,genPass) {
+     //append required parameters to the front of the password
+     passLen = passLen - genPass.length;
+     console.log(passLen);
+     for (i=0; i< passLen; i++) {
+       randNum = randomNumber(0,params.length);
+       randFunc = params[randNum];
+       if (randFunc === 'randomNumber') {
+           genPass += randomNumber(1,9);
+       }
+       else if (randFunc === 'randomLetter') {
+           genPass += randomLetter(upper=false);
+       }
+       else if (randFunc === 'randomSpecial') {
+           genPass += randomSpecial();
+       }
+       else if (randFunc === 'randomLetterUpper') {
+           genPass += randomLetter(upper=true);
+       }
+       else {
+         console.log('Problem getting parameters in generatePassword function');
+       }
+     } 
+     return genPass
 }
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(15);
+  var password = userPrompts();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
   console.log('button clicked');
 }
-
-
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);;
+generateBtn.addEventListener("click", writePassword);
 
